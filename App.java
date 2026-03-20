0c5392c32402da64e89a9260b84fc3cfce214b8e
vini <vinicius@example.com>
2026-03-20 09:53:19 -0300

    Add micro console interface

--- App.java
+++ App.java
@@ -11,7 +11,17 @@ public class App {
         recursivo = new Recursivo();
         boolean cont = true;
 
+        System.out.println("===============================");
+        System.out.println("          Hello, DEV!");
+        System.out.println("===============================");
+        System.out.println("");
+        System.out.println("Este é um script de ordenação e recursividade, feito para exemplificar o funcionamento de ambos os conceitos.");
+        System.out.println("Estes scripts foram criados na aula de Estrutura de Dados do curso de Análise e Desenvolvimento de Sistemas do IFPR, ministrada pelo professor Elvis.");
+        System.out.println("");
+        System.out.println("===============================");
+        
         while (cont) {
+            System.out.println("");
             System.out.println("Qual ação vc quer executar?");
             System.out.println("1 = Script de Ordenação");
             System.out.println("2 = Script de Recursividade");
@@ -22,29 +32,30 @@ public class App {
                 case 1:
                     ordenacao();
                     break;
-                case 2:
-                    recurividade();
-                    break;
-                case 3:
-                    cont = false;
-                    break;
-            
-                default:
+                    case 2:
+                        recurividade();
+                        break;
+                        case 3:
+                            cont = false;
+                            break;
+                            
+                            default:
                     break;
             }
         }
     }
-    
+        
     public static void recurividade() {
+        System.out.println("");
         System.out.println("Qual número vc quer dobrar?");
         int dNum = input.nextInt();
+        System.out.println("");
         System.out.println("Em qual número vc quer que pare?");
         recursivo.imprimeDobro(dNum, input.nextInt());        
         System.out.println("");
     }
     
     public static void ordenacao() {
-        System.out.println("Hello, DEV!");
         long tempo;
         // int v[] = {32,21,13,44,19,85};
         
